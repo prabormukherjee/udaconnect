61e4ea16abbf192f6912c85197358ecd01b8fa2f
prabormukherjee <prabor1@gmail.com>
2021-10-12 14:12:12 +0530

    added connections api

---
+++ modules/connections_api/app/connections/__init__.py
@@ -0,0 +1,8 @@
+from app.connections.models import Connection  # noqa
+from app.connections.schemas import ConnectionSchema  # noqa
+
+
+def register_routes(api, app, root="api"):
+    from app.connections.controllers import api as connections_api
+
+    api.add_namespace(connections_api, path=f"/{root}")
